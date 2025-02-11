1eef88592569110e4da2bf5747b9972e1ea96c1a
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-11 00:45:05 +0000

    Ported Elgot structure to chosen finite coproducts

--- Discretion/Elgot/Category.lean
+++ Discretion/Elgot/Category.lean
@@ -9,106 +9,108 @@ open Limits
 
 open MorphismProperty
 
+open ChosenFiniteCoproducts
+
 -- TODO: port to chosen coproducts...
 
-class Iterate (C : Type u) [Category C] [HasBinaryCoproducts C] where
-  iterate {X Y : C} : (X ⟶ Y ⨿ X) → (X ⟶ Y)
-  fixpoint {X Y : C} {f : X ⟶ Y ⨿ X}
-    : f ≫ coprod.desc (𝟙 Y) (iterate f) = iterate f
+class Iterate (C : Type u) [Category C] [ChosenFiniteCoproducts C] where
+  iterate {X Y : C} : (X ⟶ Y ⊕ₒ X) → (X ⟶ Y)
+  fixpoint {X Y : C} {f : X ⟶ Y ⊕ₒ X}
+    : f ≫ desc (𝟙 Y) (iterate f) = iterate f
 
-def iterate {C : Type u} [Category C] [HasBinaryCoproducts C] [Iterate C] {X Y : C}
-  : (X ⟶ Y ⨿ X) → (X ⟶ Y) := Iterate.iterate
+def iterate {C : Type u} [Category C] [ChosenFiniteCoproducts C] [Iterate C] {X Y : C}
+  : (X ⟶ Y ⊕ₒ X) → (X ⟶ Y) := Iterate.iterate
 
-class Iterate.Uniform (C : Type u) [Category C] [HasBinaryCoproducts C] [Iterate C]
+class Iterate.Uniform (C : Type u) [Category C] [ChosenFiniteCoproducts C] [Iterate C]
   (W : MorphismProperty C) : Prop where
-  uniform {X Y : C} {f : Y ⟶ Z ⨿ Y} {g : X ⟶ Z ⨿ X} {h : X ⟶ Y}
-    : W h → h ≫ f = g ≫ coprod.map (𝟙 Z) h → h ≫ iterate f = iterate g
+  uniform {X Y : C} {f : Y ⟶ Z ⊕ₒ Y} {g : X ⟶ Z ⊕ₒ X} {h : X ⟶ Y}
+    : W h → h ≫ f = g ≫ ((𝟙 Z) ⊕ₕ h) → h ≫ iterate f = iterate g
 
 -- Part 1 of Lemma 31 of Goncharov and Schröder (2018, Guarded Traced Categories)
-theorem Iterate.Uniform.squaring {C : Type u} [Category C] [HasBinaryCoproducts C] [Iterate C]
+theorem Iterate.Uniform.squaring {C : Type u} [Category C] [ChosenFiniteCoproducts C] [Iterate C]
   {W : MorphismProperty C} [W.ContainsBinaryCoproducts] [U : Iterate.Uniform C W]
-  (codiagonal : ∀{X Y : C} {f : X ⟶ (Y ⨿ X) ⨿ X},
-    iterate (iterate f) = iterate (f ≫ coprod.desc (𝟙 (Y ⨿ X)) coprod.inr))
-  {X Y : C} {f : X ⟶ Y ⨿ X} : iterate (f ≫ coprod.desc coprod.inl f) = iterate f := by
+  (codiagonal : ∀{X Y : C} {f : X ⟶ (Y ⊕ₒ X) ⊕ₒ X},
+    iterate (iterate f) = iterate (f ≫ desc (𝟙 (Y ⊕ₒ X)) (inr _ _)))
+  {X Y : C} {f : X ⟶ Y ⊕ₒ X} : iterate (f ≫ desc (inl _ _) f) = iterate f := by
   generalize hw
     : (
-      coprod.desc
-        (f ≫ coprod.map (𝟙 Y) coprod.inr ≫ coprod.inl)
-        (f ≫ coprod.map coprod.inl coprod.inl)
-      : X ⨿ X ⟶ (Y ⨿ (X ⨿ X)) ⨿ (X ⨿ X)
+      desc
+        (f ≫ ((𝟙 Y) ⊕ₕ inr _ _) ≫ inl _ _)
+        (f ≫ ((inl _ _) ⊕ₕ (inl _ _)))
+      : X ⊕ₒ X ⟶ (Y ⊕ₒ (X ⊕ₒ X)) ⊕ₒ (X ⊕ₒ X)
     )
     = w
   calc
-  _ = coprod.inr ≫ iterate (iterate w) := by
+  _ = inr _ _ ≫ iterate (iterate w) := by
     have u
-      : coprod.inr ≫ iterate w = (f ≫ coprod.desc coprod.inl f) ≫ coprod.map (𝟙 Y) coprod.inr :=
+      : inr _ _ ≫ iterate w = (f ≫ desc (inl _ _) f) ≫ ((𝟙 Y) ⊕ₕ inr _ _) :=
       calc
-      _ = coprod.inr ≫ w ≫ coprod.desc (𝟙 _) (iterate w) := by rw [fixpoint]
-      _ = f ≫ coprod.desc coprod.inl (coprod.inl ≫ iterate w) := by simp [<-hw]
-      _ = f ≫ coprod.desc coprod.inl (coprod.inl ≫ w ≫ coprod.desc (𝟙 _) (iterate w))
+      _ = inr _ _ ≫ w ≫ desc (𝟙 _) (iterate w) := by rw [fixpoint]
+      _ = f ≫ desc (inl _ _) (inl _ _ ≫ iterate w) := by simp [<-hw]
+      _ = f ≫ desc (inl _ _) (inl _ _ ≫ w ≫ desc (𝟙 _) (iterate w))
         := by rw [fixpoint]
-      _ = f ≫ coprod.desc coprod.inl f ≫ coprod.map (𝟙 _) coprod.inr := by simp [<-hw]
+      _ = f ≫ desc (inl _ _) f ≫ ((𝟙 _) ⊕ₕ inr _ _) := by simp [<-hw]; congr; simp
       _ = _ := by rw [Category.assoc]
       ;
     rw [U.uniform inr_mem u]
-  _ = coprod.inr ≫ iterate (w ≫ coprod.desc (𝟙 _) coprod.inr) := by rw [codiagonal]
-  _ = coprod.inr
-      ≫ iterate (coprod.desc (f ≫ coprod.map (𝟙 Y) coprod.inr) (f ≫ coprod.map (𝟙 Y) coprod.inl))
-        := by simp [<-hw, <-coprod.desc_comp_inl_comp_inr]
-  _ = coprod.inr ≫ codiag _ ≫ iterate f := by
+  _ = inr _ _ ≫ iterate (w ≫ desc (𝟙 _) (inr _ _)) := by rw [codiagonal]
+  _ = inr _ _ ≫ iterate (desc (f ≫ ((𝟙 Y) ⊕ₕ inr _ _)) (f ≫ ((𝟙 Y) ⊕ₕ inl _ _)))
+        := by simp [<-hw]; congr <;> simp
+  _ = inr _ _ ≫ desc (𝟙 _) (𝟙 _) ≫ iterate f := by
     have u
-      : codiag _ ≫ f
-      = coprod.desc
-          (f ≫ coprod.map (𝟙 Y) coprod.inr)
-          (f ≫ coprod.map (𝟙 Y) coprod.inl)
-        ≫ coprod.map (𝟙 _) (codiag _)
+      : desc (𝟙 _) (𝟙 _) ≫ f
+      = desc
+          (f ≫ ((𝟙 Y) ⊕ₕ inr _ _))
+          (f ≫ ((𝟙 Y) ⊕ₕ inl _ _))
+        ≫ ((𝟙 _) ⊕ₕ (desc (𝟙 _) (𝟙 _)))
       := by simp
-    rw [U.uniform codiag_mem u]
+    rw [U.uniform ?c u]
+    apply coprod_desc_mem <;> apply id_mem
   _ = _ := by simp
 
 -- Part 2 of Lemma 32 of Goncharov and Schröder (2018, Guarded Traced Categories)
-theorem Iterate.Uniform.dinaturality {C : Type u} [Category C] [HasBinaryCoproducts C] [Iterate C]
+theorem Iterate.Uniform.dinaturality {C : Type u} [Category C] [ChosenFiniteCoproducts C] [Iterate C]
   {W : MorphismProperty C} [W.ContainsBinaryCoproducts] [U : Iterate.Uniform C W]
-  (squaring : ∀{X Y : C} {f : X ⟶ Y ⨿ X}, iterate (f ≫ coprod.desc coprod.inl f) = iterate f)
-  {X Y Z : C} {f : X ⟶ Y ⨿ Z} {g : Z ⟶ Y ⨿ X}
-  : f ≫ coprod.desc (𝟙 Y) (iterate (g ≫ coprod.desc coprod.inl f))
-      = iterate (f ≫ coprod.desc coprod.inl g)
+  (squaring : ∀{X Y : C} {f : X ⟶ Y ⊕ₒ X}, iterate (f ≫ desc (inl _ _) f) = iterate f)
+  {X Y Z : C} {f : X ⟶ Y ⊕ₒ Z} {g : Z ⟶ Y ⊕ₒ X}
+  : f ≫ desc (𝟙 Y) (iterate (g ≫ desc (inl _ _) f))
+      = iterate (f ≫ desc (inl _ _) g)
   := by
   generalize hh
-    : coprod.desc (f ≫ coprod.map (𝟙 Y) coprod.inr) (g ≫ coprod.map (𝟙 Y) coprod.inl) = h
-  have h1 : coprod.inl ≫ iterate h = iterate (f ≫ coprod.desc coprod.inl g)
+    : desc (f ≫ ((𝟙 Y) ⊕ₕ (inr _ _))) (g ≫ ((𝟙 Y) ⊕ₕ inl _ _)) = h
+  have h1 : inl _ _ ≫ iterate h = iterate (f ≫ desc (inl _ _) g)
     := by
     rw [<-squaring (f := h)]
     apply U.uniform inl_mem
     simp [<-hh]
-  have h2 : coprod.inr ≫ iterate h = iterate (g ≫ coprod.desc coprod.inl f)
+  have h2 : inr _ _ ≫ iterate h = iterate (g ≫ desc (inl _ _) f)
     := by
     rw [<-squaring (f := h)]
     apply U.uniform inr_mem
     simp [<-hh]
   apply Eq.symm
   calc
-    _ = coprod.inl ≫ iterate h := h1.symm
-    _ = coprod.inl ≫ h ≫ coprod.desc (𝟙 _) (iterate h) := by rw [fixpoint]
-    _ = f ≫ coprod.desc (𝟙 _) (coprod.inr ≫ iterate h) := by simp [<-hh]
+    _ = inl _ _ ≫ iterate h := h1.symm
+    _ = inl _ _ ≫ h ≫ desc (𝟙 _) (iterate h) := by rw [fixpoint]
+    _ = f ≫ desc (𝟙 _) (inr _ _ ≫ iterate h) := by simp [<-hh]
     _ = _ := by rw [h2]
 
-class Iterate.Conway (C : Type u) [Category C] [HasBinaryCoproducts C] [Iterate C] : Prop
+class Iterate.Conway (C : Type u) [Category C] [ChosenFiniteCoproducts C] [Iterate C] : Prop
   where
-  naturality {X Y Z : C} {f : X ⟶ Y ⨿ X} {g : Y ⟶ Z}
-    : iterate (f ≫ coprod.map g (𝟙 X)) = (iterate f) ≫ g
-  dinaturality {X Y Z : C} {f : X ⟶ Y ⨿ Z} {g : Z ⟶ Y ⨿ X}
-    : f ≫ coprod.desc (𝟙 Y) (iterate (g ≫ coprod.desc coprod.inl f))
-      = iterate (f ≫ coprod.desc coprod.inl g)
-  codiagonal {X Y : C} {f : X ⟶ (Y ⨿ X) ⨿ X}
-    : iterate (iterate f) = iterate (f ≫ coprod.desc (𝟙 (Y ⨿ X)) coprod.inr)
+  naturality {X Y Z : C} {f : X ⟶ Y ⊕ₒ X} {g : Y ⟶ Z}
+    : iterate (f ≫ (g ⊕ₕ (𝟙 X))) = (iterate f) ≫ g
+  dinaturality {X Y Z : C} {f : X ⟶ Y ⊕ₒ Z} {g : Z ⟶ Y ⊕ₒ X}
+    : f ≫ desc (𝟙 Y) (iterate (g ≫ desc (inl _ _) f))
+      = iterate (f ≫ desc (inl _ _) g)
+  codiagonal {X Y : C} {f : X ⟶ (Y ⊕ₒ X) ⊕ₒ X}
+    : iterate (iterate f) = iterate (f ≫ desc (𝟙 (Y ⊕ₒ X)) (inr _ _))
 
-theorem Iterate.Uniform.conway {C : Type u} [Category C] [HasBinaryCoproducts C] [Iterate C]
+theorem Iterate.Uniform.conway {C : Type u} [Category C] [ChosenFiniteCoproducts C] [Iterate C]
   {W : MorphismProperty C} [W.ContainsBinaryCoproducts] [U : Iterate.Uniform C W]
-  (naturality : ∀{X Y Z : C} {f : X ⟶ Y ⨿ X} {g : Y ⟶ Z},
-    iterate (f ≫ coprod.map g (𝟙 X)) = (iterate f) ≫ g)
-  (codiagonal : ∀{X Y : C} {f : X ⟶ (Y ⨿ X) ⨿ X},
-    iterate (iterate f) = iterate (f ≫ coprod.desc (𝟙 (Y ⨿ X)) coprod.inr))
+  (naturality : ∀{X Y Z : C} {f : X ⟶ Y ⊕ₒ X} {g : Y ⟶ Z},
+    iterate (f ≫ (g ⊕ₕ (𝟙 X))) = (iterate f) ≫ g)
+  (codiagonal : ∀{X Y : C} {f : X ⟶ (Y ⊕ₒ X) ⊕ₒ X},
+    iterate (iterate f) = iterate (f ≫ desc (𝟙 (Y ⊕ₒ X)) (inr _ _)))
   : Iterate.Conway C where
   naturality := naturality
   codiagonal := codiagonal
--- Discretion/MorphismProperty/BinaryProducts.lean
+++ Discretion/MorphismProperty/BinaryProducts.lean
@@ -4,92 +4,45 @@ import Mathlib.CategoryTheory.MorphismProperty.Composition
 import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
 import Mathlib.CategoryTheory.Limits.Shapes.IsTerminal
 
+import Discretion.ChosenFiniteCoproducts
+
 namespace CategoryTheory.MorphismProperty
 
-open Limits
+open ChosenFiniteCoproducts
 
-variable {C} [Category C]
-
-class ContainsProjections (W : MorphismProperty C) : Prop where
-  fst_mem : ∀{X Y : C} [HasBinaryProduct X Y], W (prod.fst : X ⨯ Y ⟶ X)
-  snd_mem : ∀{X Y : C} [HasBinaryProduct X Y], W (prod.snd : X ⨯ Y ⟶ Y)
-
-theorem fst_mem {W : MorphismProperty C} [ContainsProjections W] {X Y : C} [HasBinaryProduct X Y]
-  : W (prod.fst : X ⨯ Y ⟶ X) := ContainsProjections.fst_mem
-
-theorem snd_mem {W : MorphismProperty C} [ContainsProjections W] {X Y : C} [HasBinaryProduct X Y]
-  : W (prod.snd : X ⨯ Y ⟶ Y) := ContainsProjections.snd_mem
-
-class ContainsProdLift (W : MorphismProperty C) : Prop where
-  prod_lift_mem : ∀{X Y Z : C} {f : X ⟶ Y} {g : X ⟶ Z} [HasBinaryProduct Y Z],
-    W f → W g → W (prod.lift f g)
-
-theorem prod_lift_mem {W : MorphismProperty C} [ContainsProdLift W] {X Y Z : C}
-  {f : X ⟶ Y} {g : X ⟶ Z} [HasBinaryProduct Y Z] (hf : W f) (hg : W g) : W (prod.lift f g)
-  := ContainsProdLift.prod_lift_mem hf hg
-
-class ContainsBinaryProducts (W : MorphismProperty C)
-  extends ContainsProjections W, ContainsProdLift W, IsMultiplicative W : Prop
-
-theorem prod_map_mem {W : MorphismProperty C} [ContainsBinaryProducts W]
-  {X Y X' Y' : C} {f : X ⟶ X'} {g : Y ⟶ Y'} [HasBinaryProduct X Y] [HasBinaryProduct X' Y']
-  (hf : W f) (hg : W g) : W (prod.map f g) := by
-  rw [<-prod.lift_fst_comp_snd_comp]
-  apply prod_lift_mem <;> apply comp_mem <;> first | assumption | apply fst_mem | apply snd_mem
+variable {C} [Category C] [ChosenFiniteCoproducts C]
 
 class ContainsInjections (W : MorphismProperty C) : Prop where
-  inl_mem : ∀{X Y : C} [HasBinaryCoproduct X Y], W (coprod.inl : X ⟶ X ⨿ Y)
-  inr_mem : ∀{X Y : C} [HasBinaryCoproduct X Y], W (coprod.inr : Y ⟶ X ⨿ Y)
+  inl_mem : ∀X Y : C, W (inl X Y)
+  inr_mem : ∀X Y : C, W (inr X Y)
 
-theorem inl_mem {W : MorphismProperty C} [ContainsInjections W] {X Y : C} [HasBinaryCoproduct X Y]
-  : W (coprod.inl : X ⟶ X ⨿ Y) := ContainsInjections.inl_mem
+theorem inl_mem {W : MorphismProperty C} [ContainsInjections W] {X Y : C}
+  : W (inl X Y) := ContainsInjections.inl_mem X Y
 
-theorem inr_mem {W : MorphismProperty C} [ContainsInjections W] {X Y : C} [HasBinaryCoproduct X Y]
-  : W (coprod.inr : Y ⟶ X ⨿ Y) := ContainsInjections.inr_mem
+theorem inr_mem {W : MorphismProperty C} [ContainsInjections W] {X Y : C}
+  : W (inr X Y) := ContainsInjections.inr_mem X Y
 
 class ContainsCoprodDesc (W : MorphismProperty C) : Prop where
-  coprod_desc_mem : ∀{X Y Z : C} {f : X ⟶ Z} {g : Y ⟶ Z} [HasBinaryCoproduct X Y],
-    W f → W g → W (coprod.desc f g)
+  coprod_desc_mem : ∀{X Y Z : C} {f : X ⟶ Z} {g : Y ⟶ Z}, W f → W g → W (desc f g)
 
 theorem coprod_desc_mem {W : MorphismProperty C} [ContainsCoprodDesc W] {X Y Z : C}
-  {f : X ⟶ Z} {g : Y ⟶ Z} [HasBinaryCoproduct X Y] (hf : W f) (hg : W g) : W (coprod.desc f g)
+  {f : X ⟶ Z} {g : Y ⟶ Z} (hf : W f) (hg : W g) : W (desc f g)
   := ContainsCoprodDesc.coprod_desc_mem hf hg
-
-theorem codiag_mem {W : MorphismProperty C} [ContainsCoprodDesc W] [ContainsIdentities W]
-  {X : C} [HasBinaryCoproduct X X]
-  : W (codiag X) := coprod_desc_mem (id_mem _ _) (id_mem _ _)
 
 class ContainsBinaryCoproducts (W : MorphismProperty C)
   extends ContainsInjections W, ContainsCoprodDesc W, IsMultiplicative W : Prop
 
-theorem coprod_map_mem {W : MorphismProperty C} [ContainsBinaryCoproducts W]
-  {X Y X' Y' : C} {f : X ⟶ X'} {g : Y ⟶ Y'} [HasBinaryCoproduct X Y] [HasBinaryCoproduct X' Y']
-  (hf : W f) (hg : W g) : W (coprod.map f g) := by
-  rw [<-coprod.desc_comp_inl_comp_inr]
+theorem addHom_mem {W : MorphismProperty C} [ContainsBinaryCoproducts W]
+  {X Y X' Y' : C} {f : X ⟶ X'} {g : Y ⟶ Y'}
+  (hf : W f) (hg : W g) : W (f ⊕ₕ g) := by
+  rw [addHom]
   apply coprod_desc_mem <;> apply comp_mem <;> first | assumption | apply inl_mem | apply inr_mem
 
-class ContainsTerminal (W : MorphismProperty C) : Prop where
-  terminal_mem : ∀(X : C) [HasTerminal C], W (terminal.from X)
-
 class ContainsInitial (W : MorphismProperty C) : Prop where
-  initial_mem : ∀(X : C) [HasInitial C], W (initial.to X)
-
-class ContainsProducts (W : MorphismProperty C)
-  extends ContainsTerminal W, ContainsBinaryProducts W
-
-instance {W : MorphismProperty C} [ContainsTerminal W] [ContainsBinaryProducts W]
-  : ContainsProducts W := ⟨⟩
+  initial_mem : ∀(X : C), W (fromZero X)
 
 class ContainsCoproducts (W : MorphismProperty C)
   extends ContainsInitial W, ContainsBinaryCoproducts W
 
 instance {W : MorphismProperty C} [ContainsInitial W] [ContainsBinaryCoproducts W]
   : ContainsCoproducts W := ⟨⟩
-
--- class ContainsTerminals (W : MorphismProperty C) : Prop where
---   terminal_mem : ∀(X : C), (h : IsTerminal X) → ∀Y, W (h.from Y)
-
--- class ContainsInitials (W : MorphismProperty C) : Prop where
---   initial_mem : ∀(X : C), (h : IsInitial X) → ∀Y, W (h.to Y)
-
--- TODO: inf and sup instances; top instances
