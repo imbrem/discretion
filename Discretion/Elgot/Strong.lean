54fdc60c522e6222d7d0b9755f944bdac04419d8
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-11 00:47:26 +0000

    Strong Elgot categories

--- Discretion/Elgot/Strong.lean
+++ Discretion/Elgot/Strong.lean
@@ -1,33 +1,33 @@
--- import Discretion.Elgot.Category
--- import Discretion.Premonoidal.Distributive
+import Discretion.Elgot.Category
+import Discretion.Distributive.Category
 
--- namespace CategoryTheory
+namespace CategoryTheory
 
--- open MonoidalCategory
+open MonoidalCategory
 
--- open Monoidal
+open Monoidal
 
--- open Limits
+open ChosenFiniteCoproducts
 
--- variable {C : Type u} [Category C] [MonoidalCategoryStruct C] [HasBinaryCoproducts C] [Iterate C]
+variable {C : Type u} [Category C] [MonoidalCategoryStruct C] [ChosenFiniteCoproducts C] [Iterate C]
 
--- class IsStrongElgot (C : Type _)
---   [Category C] [MonoidalCategoryStruct C] [HasBinaryCoproducts C] [Iterate C]
---   extends IsPremonoidal C, IsDistributive C, Iterate.Conway C : Prop
---   where
---   iterate_whiskerLeft : ∀ {X Y : C} (Z : C) (f : X ⟶ Y ⨿ X),
---     iterate ((Z ◁ f) ≫ inv (δl_ Z Y X)) = Z ◁ iterate f
---   iterate_whiskerRight : ∀ {X Y : C} (f : X ⟶ Y ⨿ X) (Z : C),
---     iterate ((f ▷ Z) ≫ inv (δr_ Y X Z)) = iterate f ▷ Z
+class IsStrongElgot (C : Type _)
+  [Category C] [MonoidalCategoryStruct C] [ChosenFiniteCoproducts C] [Iterate C]
+  extends IsPremonoidal C, DistributiveCategory C, Iterate.Conway C : Prop
+  where
+  iterate_whiskerLeft : ∀ {X Y : C} (Z : C) (f : X ⟶ Y ⊕ₒ X),
+    iterate ((Z ◁ f) ≫ inv (δl_ Z Y X)) = Z ◁ iterate f
+  iterate_whiskerRight : ∀ {X Y : C} (f : X ⟶ Y ⊕ₒ X) (Z : C),
+    iterate ((f ▷ Z) ≫ inv (δr_ Y X Z)) = iterate f ▷ Z
 
--- namespace Monoidal
+namespace Monoidal
 
--- theorem iterate_whiskerLeft [IsStrongElgot C] {X Y : C} (Z : C) (f : X ⟶ Y ⨿ X)
---   : iterate ((Z ◁ f) ≫ inv (δl_ Z Y X)) = Z ◁ iterate f
---   := IsStrongElgot.iterate_whiskerLeft Z f
+theorem iterate_whiskerLeft [IsStrongElgot C] {X Y : C} (Z : C) (f : X ⟶ Y ⊕ₒ X)
+  : iterate ((Z ◁ f) ≫ inv (δl_ Z Y X)) = Z ◁ iterate f
+  := IsStrongElgot.iterate_whiskerLeft Z f
 
--- theorem iterate_whiskerRight [IsStrongElgot C] {X Y : C} (f : X ⟶ Y ⨿ X) (Z : C)
---   : iterate ((f ▷ Z) ≫ inv (δr_ Y X Z)) = iterate f ▷ Z
---   := IsStrongElgot.iterate_whiskerRight f Z
+theorem iterate_whiskerRight [IsStrongElgot C] {X Y : C} (f : X ⟶ Y ⊕ₒ X) (Z : C)
+  : iterate ((f ▷ Z) ≫ inv (δr_ Y X Z)) = iterate f ▷ Z
+  := IsStrongElgot.iterate_whiskerRight f Z
 
--- end Monoidal
+end Monoidal
