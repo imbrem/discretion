1ec9dda1babbd37c674a0a1bf54166558df6b0ae
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-13 15:30:49 +0000

    Simplified effectful category definition

--- Discretion/EffectSystem/Basic.lean
+++ Discretion/EffectSystem/Basic.lean
@@ -1,32 +1,48 @@
 import Discretion.Quant.Basic
 
 class HasCommRel (ε : Type u) [PartialOrder ε] [OrderBot ε] : Sort _ where
-  slides : ε → ε → Prop
-  slides_anti : ∀{e₁ e₂ e₁' e₂'}, e₁ ≤ e₁' → e₂ ≤ e₂' → slides e₁' e₂' → slides e₁ e₂
-  slides_bot_left : ∀e, slides ⊥ e
-  slides_bot_right : ∀e, slides e ⊥
+  right_mover : ε → ε → Prop
+  right_mover_anti : ∀{e₁ e₂ e₁' e₂'}, e₁ ≤ e₁' → e₂ ≤ e₂' → right_mover e₁' e₂' → right_mover e₁ e₂
+  right_mover_bot : ∀e, right_mover ⊥ e
+  right_mover_rel_bot : ∀e, right_mover e ⊥
 
 namespace HasCommRel
 
 variable {ε} [PartialOrder ε] [BoundedOrder ε] [HasCommRel ε]
 
-scoped infixr:50 " >‖> " => slides
+scoped infixr:50 " ⇀ " => right_mover
 
-def commutes (l r : ε) : Prop := slides l r ∧ slides r l
+abbrev left_mover (l r : ε) : Prop := r ⇀ l
 
-scoped infixr:50 " ‖ " => commutes
+scoped infixr:50 " ↽ " => left_mover
 
-theorem commutes_def {l r : ε} : l ‖ r ↔ l >‖> r ∧ r >‖> l := Iff.rfl
+theorem left_mover_bot (e : ε) : ⊥ ↽ e := right_mover_rel_bot e
 
-theorem commutes_symm_iff {l r : ε} : l ‖ r ↔ r ‖ l := by simp [commutes_def, and_comm]
+theorem left_mover_rel_bot (e : ε) : e ↽ ⊥ := right_mover_bot e
 
-theorem commutes_symm {l r : ε} : l ‖ r → r ‖ l := commutes_symm_iff.mp
+theorem left_mover_anti {e₁ e₂ e₁' e₂' : ε} (h₁ : e₁ ≤ e₁') (h₂ : e₂ ≤ e₂') : e₁' ↽ e₂' → e₁ ↽ e₂
+  := right_mover_anti h₂ h₁
 
-theorem commutes_bot_left {r : ε} : (⊥ : ε) ‖ r := ⟨slides_bot_left r, slides_bot_right r⟩
+abbrev commutes (l r : ε) : Prop := l ⇀ r ∧ l ↽ r
 
-theorem commutes_bot_right {l : ε} : l ‖ (⊥ : ε) := commutes_symm commutes_bot_left
+scoped infixr:50 " ⇌ " => commutes
 
-theorem slides_self_iff {e : ε} : e >‖> e ↔ e ‖ e := by simp [commutes_def]
+theorem commutes_def {l r : ε} : l ⇌ r ↔ l ⇀ r ∧ r ⇀ l := Iff.rfl
+
+theorem commutes_anti {l r l' r' : ε} (h₁ : l ≤ l') (h₂ : r ≤ r') (hc : l' ⇌ r') : l ⇌ r :=
+  ⟨right_mover_anti h₁ h₂ hc.1, left_mover_anti h₁ h₂ hc.2⟩
+
+theorem commutes_symm_iff {l r : ε} : l ⇌ r ↔ r ⇌ l := by simp [commutes_def, and_comm]
+
+theorem commutes_symm {l r : ε} : l ⇌ r → r ⇌ l := commutes_symm_iff.mp
+
+theorem commutes_bot_left {r : ε} : (⊥ : ε) ⇌ r := ⟨right_mover_bot r, left_mover_bot r⟩
+
+theorem commutes_bot_right {l : ε} : l ⇌ (⊥ : ε) := commutes_symm commutes_bot_left
+
+theorem right_mover_self_iff {e : ε} : e ⇀ e ↔ e ⇌ e := by simp [commutes_def]
+
+theorem left_mover_self_iff {e : ε} : e ↽ e ↔ e ⇌ e := right_mover_self_iff
 
 end HasCommRel
 
--- Discretion/Premonoidal/Effectful.lean
+++ Discretion/Premonoidal/Effectful.lean
@@ -20,105 +20,41 @@ open MorphismProperty
 
 open HasCommRel
 
--- TODO: Effectful ==> EffectfulCategory; then Effectful is purely an algebra on E?
+class RightMover [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
+  (L R : MorphismProperty C) where
+  left_fwd : ∀ {X Y X' Y' : C} (f : X ⟶ Y) (g : X' ⟶ Y'), L f → R g → f ⋉ g ≤ f ⋊ g
+  right_fwd : ∀ {X Y X' Y' : C} (f : X ⟶ Y) (g : X' ⟶ Y'), L f → R g → g ⋊ f ≤ g ⋉ f
 
-class EffectfulStruct
+abbrev LeftMover [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
+  (L R : MorphismProperty C) := RightMover R L
+
+theorem Commutes.of_left_right
+  [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
+  (L R : MorphismProperty C) [RightMover L R] [LeftMover L R]
+  : Commutes L R where
+  left_sliding f g hf hg
+    := le_antisymm (RightMover.left_fwd f g hf hg) (RightMover.right_fwd g f hg hf)
+  right_sliding f g hf hg
+    := le_antisymm (RightMover.left_fwd g f hg hf) (RightMover.right_fwd f g hf hg)
+
+theorem Commutes.right
+  [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
+  (L R : MorphismProperty C) [hC : Commutes L R] : RightMover L R where
+  left_fwd f g hf hg := le_of_eq (Commutes.left_sliding f g hf hg)
+  right_fwd f g hf hg := le_of_eq (Commutes.right_sliding f g hf hg).symm
+
+theorem Commutes.left
+  [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
+  (L R : MorphismProperty C) [hC : Commutes L R] : LeftMover L R
+  := right R L (hC := hC.symm)
+
+class EffectfulCategory
   (C : Type v) [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
-  (E : Type u) [PartialOrder E] [BoundedOrder E] [HasCommRel E]
-  : Type _ where
+  [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
+  (E : Type u) [EffectSystem E] where
   eff : E →o MorphismProperty C
   eff_top : eff ⊤ = ⊤
   eff_braided : ∀e, (eff e).IsBraided
-
-open EffectfulStruct
-
-class Slides [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
-  (L R : MorphismProperty C) where
-  left_sliding_fwd : ∀ {X Y X' Y' : C} (f : X ⟶ Y) (g : X' ⟶ Y'), L f → R g → f ⋉ g ≤ f ⋊ g
-  right_sliding_fwd : ∀ {X Y X' Y' : C} (f : X ⟶ Y) (g : X' ⟶ Y'), L f → R g → g ⋊ f ≤ g ⋉ f
-
-class EnrichedEffectful
-  (C : Type v) [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
-  [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
-  (E : Type u) [PartialOrder E] [BoundedOrder E] [HasCommRel E]
-  [EffectfulStruct C E]
-  : Prop
-  where
-  eff_slides : ∀e₁ e₂ : E, e₁ ‖ e₂ → Slides (C := C) (eff e₁) (eff e₂)
-
-class Effectful
-  (C : Type v) [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
-  (E : Type u) [PartialOrder E] [BoundedOrder E] [HasCommRel E]
-  [EffectfulStruct C E]
-  : Prop where
-  eff_commutes : ∀e₁ e₂ : E, e₁ ‖ e₂ → Commutes (C := C) (eff e₁) (eff e₂)
-
--- attribute [simp] Effectful.eff_top
-
--- open HasPQuant HasQuant
-
--- class PSubEffectful
---   (C : Type v)
---   [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C] [∀X Y : C, LE (X ⟶ Y)]
---   [HasQuant C] [CopyDrop C]
---   (E : Type u) [PartialOrder E] [BoundedOrder E] [HasCommRel E] [HasPQuant E]
---   extends Effectful C E where
---   eff_has_copy_drop : ∀e : E, (eff e).HasCopyDrop
---   eff_fusable : ∀e : E, .fuse ≤ pquant e → (eff e).Fusable
---   eff_duplicable : ∀e : E, .dup ≤ pquant e → (eff e).Duplicable
---   eff_addable : ∀e : E, .add ≤ pquant e → (eff e).Addable
---   eff_removable : ∀e : E, .rem ≤ pquant e → (eff e).Removable
-
--- class SubEffectful
---   (C : Type v)
---   [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
---   [HasQuant C] [CopyDrop C]
---   (E : Type u) [PartialOrder E] [BoundedOrder E] [HasCommRel E] [HasQuant E]
---   extends Effectful C E where
---   eff_copyable : ∀e : E, .copy ≤ HasQuant.quant e → (eff e).Copyable
---   eff_discardable : ∀e : E, .del ≤ HasQuant.quant e → (eff e).Discardable
-
--- instance SubEffectful.ofPSubEffectful
---   {C : Type v}
---   [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
---   [HasQuant C] [CopyDrop C] [∀X Y : C, PartialOrder (X ⟶ Y)]
---   {E : Type u} [PartialOrder E] [BoundedOrder E] [HasCommRel E] [HasPQuant E]
---   [PSubEffectful C E]
---   : SubEffectful C E where
---   eff_copyable e h :=
---     let fusable := PSubEffectful.eff_fusable e
---       (PQuant.fuse_le_copy.trans ((PQuant.coe_mono h).trans (PQuant.q_le _)))
---     let duplicable := PSubEffectful.eff_duplicable e
---       (PQuant.dup_le_copy.trans ((PQuant.coe_mono h).trans (PQuant.q_le _)))
---     Copyable.of_fusable_duplicable (hF := fusable) (hD := duplicable)
---   eff_discardable e h :=
---     let addable := PSubEffectful.eff_addable e
---       (PQuant.add_le_del.trans ((PQuant.coe_mono h).trans (PQuant.q_le _)))
---     let removable := PSubEffectful.eff_removable e
---       (PQuant.rem_le_del.trans ((PQuant.coe_mono h).trans (PQuant.q_le _)))
---     Discardable.of_addable_removable (hA := addable) (hR := removable)
-
--- -- TODO: every subeffectful category has a coherent comonoid supply, since monoidal is rel + aff
-
--- class OPGSCategory.{v, u}
---   (C : Type v) [Category C] [∀X Y : C, PartialOrder (X ⟶ Y)]
---   (E : Type u) [EffectSystem E]
---   extends
---   CompMonotone C, -- Is a 2-poset
---   MonoidalCategoryStruct C, IsPremonoidal C, WhiskeringMonotone C, -- Is premonoidal
---   BraidedCategoryStruct C, IsSymmetric C, -- Is symmetric
---   CopyQuant C, ComonoidSupply C, -- Has a comonoid supply
---   PSubEffectful C E -- Is subeffectful w.r.t. E
---   where
-
--- instance OPGSCategory.instMk
---   {C : Type v} [Category C] [∀X Y : C, PartialOrder (X ⟶ Y)]
---   {E : Type u} [EffectSystem E]
---   [CompMonotone C] [MonoidalCategoryStruct C] [IsPremonoidal C] [WhiskeringMonotone C]
---   [BraidedCategoryStruct C] [IsSymmetric C] [CopyQuant C] [ComonoidSupply C]
---   [PSubEffectful C E]
---   : OPGSCategory C E where
-
--- -- TODO: actually _define_ some OPGS categories
+  eff_right_mover : ∀{e e' : E}, e ⇀ e' → RightMover (eff e) (eff e')
 
 namespace Monoidal
