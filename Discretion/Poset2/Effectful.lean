e960eeba0731c12757f05c8033b009aa7fffcbe0
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-16 16:18:47 +0000

    Lemmas on centrality

--- Discretion/Poset2/Effectful.lean
+++ Discretion/Poset2/Effectful.lean
@@ -30,7 +30,7 @@ abbrev LeftMover [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, Partia
 
 theorem Commutes.of_left_right
   [Category C] [MonoidalCategoryStruct C] [O : ∀X Y : C, PartialOrder (X ⟶ Y)]
-  (L R : MorphismProperty C) [RightMover L R] [LeftMover L R]
+  (L R : MorphismProperty C) [right : RightMover L R] [left : LeftMover L R]
   : Commutes L R where
   left_sliding f g hf hg
     := le_antisymm (RightMover.left_fwd f g hf hg) (RightMover.right_fwd g f hg hf)
@@ -57,8 +57,26 @@ class Effectful2
   eff_braided : ∀e, (eff e).IsBraided
   eff_right_mover : ∀{e e' : E}, e ⇀ e' → RightMover (eff e) (eff e')
 
-abbrev Effectful2.pure [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
-  [O : ∀X Y : C, LE (X ⟶ Y)] (E : Type u) [EffectSystem E] [EC : Effectful2 C E]
-  : MorphismProperty C := EC.eff ⊥
+variable {C : Type v} [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
+  [O : ∀X Y : C, LE (X ⟶ Y)] {E : Type u} [EffectSystem E] [EC : Effectful2 C E]
+
+abbrev Effectful2.pure : MorphismProperty C := EC.eff ⊥
+
+theorem Effectful2.eff_left_mover {e e' : E} (h : e ↽ e') : LeftMover (EC.eff e) (EC.eff e')
+  := eff_right_mover h
+
+theorem Effectful2.eff_commutes {e e' : E} (h : e ⇌ e') : Commutes (EC.eff e) (EC.eff e')
+  := Commutes.of_left_right (EC.eff e) (EC.eff e')
+      (right := eff_right_mover h.1)
+      (left := eff_left_mover h.2)
+
+theorem Effectful2.pure_commutes_eff (e : E) : Commutes (EC.pure) (EC.eff e)
+  := eff_commutes commutes_bot_left
+
+theorem Effectful2.pure_central : Central (EC.pure)
+  := Central.of_commutes_top (h := by convert pure_commutes_eff ⊤; rw [EC.eff_top])
+
+theorem Effectful2.pure_hom_central {f : X ⟶ Y} (h : EC.pure f) : Central f
+  := pure_central.central h
 
 namespace Monoidal
