d92a61921afbf672085d827662097f13e97a8f59
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-02-17 21:38:36 +0000

    HasEff typeclass

--- Discretion/Poset2/Distributive.lean
+++ Discretion/Poset2/Distributive.lean
@@ -4,7 +4,51 @@ import Discretion.Poset2.Effectful
 
 namespace CategoryTheory
 
+open ChosenFiniteCoproducts
+
+open DistributiveCategory
+
+open Monoidal
+
 class Distributive2 (C : Type u)
   [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C] [ChosenFiniteCoproducts C]
   (E : Type v) [EffectSystem E] extends Effectful2 C E, DistributiveCategory C, DescMono C where
   eff_distributive: ∀e, (eff e).Distributive
+
+variable  {C : Type u}
+          [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
+          [ChosenFiniteCoproducts C] {E : Type v} [EffectSystem E] [D2 : Distributive2 C E]
+
+instance EffectfulCategory.HasEff.inl {e : E} {X Y : C} : HasEff e (inl X Y) where
+  has_eff := (D2.eff_distributive e).inl_mem X Y
+
+instance EffectfulCategory.HasEff.inr {e : E} {X Y : C} : HasEff e (inr X Y) where
+  has_eff := (D2.eff_distributive e).inr_mem X Y
+
+instance EffectfulCategory.HasEff.desc
+  {e : E} {X Y Z : C} (f : X ⟶ Z) (g : Y ⟶ Z)
+  [hf : HasEff e f] [hg : HasEff e g] : HasEff e (desc f g) where
+  has_eff := (D2.eff_distributive e).coprod_desc_mem hf.has_eff hg.has_eff
+
+instance EffectfulCategory.HasEff.addHom
+  {e : E} {X Y X' Y' : C} (f : X ⟶ X') (g : Y ⟶ Y')
+  [hf : HasEff e f] [hg : HasEff e g] : HasEff e (f ⊕ₕ g)
+  := desc _ _
+
+instance EffectfulCategory.HasEff.distl
+  {e : E} {X Y Z : C} : HasEff e ((∂L X Y Z).hom : _ ⟶ _)
+  := desc _ _
+
+instance EffectfulCategory.HasEff.distr
+  {e : E} {X Y Z : C} : HasEff e ((∂R X Y Z).hom : _ ⟶ _)
+  := desc _ _
+
+instance EffectfulCategory.HasEff.distl_inv
+  {e : E} {X Y Z : C} : HasEff e ((∂L X Y Z).inv : _ ⟶ _) where
+  has_eff := (D2.eff_distributive e).distl_inv_mem
+
+instance EffectfulCategory.HasEff.distr_inv
+  {e : E} {X Y Z : C} : HasEff e ((∂R X Y Z).inv : _ ⟶ _) where
+  has_eff := (D2.eff_distributive e).distr_inv_mem
+
+end CategoryTheory
--- Discretion/Premonoidal/Effectful.lean
+++ Discretion/Premonoidal/Effectful.lean
@@ -33,4 +33,53 @@ abbrev EffectfulCategory.pure [Category C] [MonoidalCategoryStruct C] [BraidedCa
   (E : Type u) [EffectSystem E] [EC : EffectfulCategory C E] : MorphismProperty C
   := EC.eff ⊥
 
+variable {C : Type v} [Category C] [MonoidalCategoryStruct C] [BraidedCategoryStruct C]
+  {E : Type u} [EffectSystem E] [EC : EffectfulCategory C E]
+
+class EffectfulCategory.HasEff (e : E) {X Y : C} (f : X ⟶ Y) : Prop where
+  has_eff : EC.eff e f
+
+instance EffectfulCategory.HasEff.id (e : E) {X : C} : HasEff e (𝟙 X) where
+  has_eff := (EC.eff_monoidal e).id_mem X
+
+instance EffectfulCategory.HasEff.comp {e : E} {X Y Z : C} (f : X ⟶ Y) (g : Y ⟶ Z)
+  [hf : HasEff e f] [hg : HasEff e g] : HasEff e (f ≫ g) where
+  has_eff := (EC.eff_monoidal e).comp_mem f g hf.has_eff hg.has_eff
+
+instance EffectfulCategory.HasEff.whiskerRight {e : E} {X Y Z : C} (f : X ⟶ Y)
+  [hf : HasEff e f] : HasEff e (f ▷ Z) where
+  has_eff := (EC.eff_monoidal e).whiskerRight_mem f hf.has_eff
+
+instance EffectfulCategory.HasEff.whiskerLeft {e : E} {X Y Z : C} (f : X ⟶ Y)
+  [hf : HasEff e f] : HasEff e (Z ◁ f) where
+  has_eff := (EC.eff_monoidal e).whiskerLeft_mem f hf.has_eff
+
+instance EffectfulCategory.HasEff.tensorHom [IsBinoidal C]
+  {e : E} {X Y X' Y' : C} (f : X ⟶ Y) (g : X' ⟶ Y')
+  [hf : HasEff e f] [hg : HasEff e g] : HasEff e (f ⊗ g) where
+  has_eff := have _ := (EC.eff_monoidal e); MorphismProperty.tensorHom_mem hf.has_eff hg.has_eff
+
+instance EffectfulCategory.HasEff.associator_hom {e : E} {X Y Z : C} : HasEff e (α_ X Y Z).hom where
+  has_eff := (EC.eff_monoidal e).associator_hom_mem
+
+instance EffectfulCategory.HasEff.associator_inv {e : E} {X Y Z : C} : HasEff e (α_ X Y Z).inv where
+  has_eff := (EC.eff_monoidal e).associator_inv_mem
+
+instance EffectfulCategory.HasEff.leftUnitor_hom {e : E} {X : C} : HasEff e (λ_ X).hom where
+  has_eff := (EC.eff_monoidal e).leftUnitor_hom_mem
+
+instance EffectfulCategory.HasEff.leftUnitor_inv {e : E} {X : C} : HasEff e (λ_ X).inv where
+  has_eff := (EC.eff_monoidal e).leftUnitor_inv_mem
+
+instance EffectfulCategory.HasEff.rightUnitor_hom {e : E} {X : C} : HasEff e (ρ_ X).hom where
+  has_eff := (EC.eff_monoidal e).rightUnitor_hom_mem
+
+instance EffectfulCategory.HasEff.rightUnitor_inv {e : E} {X : C} : HasEff e (ρ_ X).inv where
+  has_eff := (EC.eff_monoidal e).rightUnitor_inv_mem
+
+instance EffectfulCategory.HasEff.braiding_hom {e : E} {X Y : C} : HasEff e (σ_ X Y) where
+  has_eff := (EC.eff_braided e).braiding_hom_mem
+
+abbrev EffectfulCategory.IsPure {X Y : C} (f : X ⟶ Y) : Prop := HasEff (E := E) ⊥ f
+
 namespace Monoidal
