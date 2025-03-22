8f00105d798cb3fa668cf474dd435cc95445b820
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-03-22 23:43:07 +0000

    Commutativty helpers

--- Discretion/Poset2/Effectful.lean
+++ Discretion/Poset2/Effectful.lean
@@ -41,20 +41,115 @@ theorem Commutes.left
 
 class Effectful2
   (C : Type v) [Category C] [PremonoidalCategory C] [BraidedCategory' C]
-  (E : Type u) [EffectSystem E] extends EffectfulCategory C E, MonPoset2 C where
-  eff_right_mover : ∀{e e' : E}, e ⇀ e' → RightMover (eff e) (eff e')
+  (ε : Type u) [EffectSystem ε] extends EffectfulCategory C ε, MonPoset2 C where
+  eff_right_mover : ∀{e e' : ε}, e ⇀ e' → RightMover (eff e) (eff e')
   eff_comm h
     := Commutes.of_left_right _ _ (right := eff_right_mover h.1) (left := eff_right_mover h.2)
 
 variable {C : Type v} [Category C] [PremonoidalCategory C] [BraidedCategory' C]
-  {E : Type u} [EffectSystem E] [EC : Effectful2 C E]
+  {ε : Type u} [EffectSystem ε] [EC : Effectful2 C ε]
 
-theorem Effectful2.eff_left_mover {e e' : E} (h : e ↽ e') : LeftMover (EC.eff e) (EC.eff e')
+theorem Effectful2.eff_left_mover {e e' : ε} (h : e ↽ e') : LeftMover (EC.eff e) (EC.eff e')
   := eff_right_mover h
 
-theorem Effectful2.eff_commutes {e e' : E} (h : e ⇌ e') : Commutes (EC.eff e) (EC.eff e')
+theorem Effectful2.eff_commutes {e e' : ε} (h : e ⇌ e') : Commutes (EC.eff e) (EC.eff e')
   := Commutes.of_left_right (EC.eff e) (EC.eff e')
       (right := eff_right_mover h.1)
       (left := eff_left_mover h.2)
 
-namespace Monoidal
+theorem Effectful2.eff_left_fwd
+  {X₁ Y₁ X₂ Y₂ : C} {el er : ε} (h : el ⇀ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂) [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    f ▷ X₂ ≫ Y₁ ◁ g ↠ X₁ ◁ g ≫ f ▷ Y₂
+    := (EC.eff_right_mover h).left_fwd f g hf.has_eff hg.has_eff
+
+theorem Effectful2.eff_right_fwd
+  {X₁ Y₁ X₂ Y₂ : C} {el er : ε} (h : el ⇀ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂) [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    _ ◁ f ≫ g ▷ _ ↠ g ▷ _ ≫ _ ◁ f
+    := (EC.eff_right_mover h).right_fwd f g hf.has_eff hg.has_eff
+
+theorem Effectful2.eff_left_bwd
+  {X₁ Y₁ X₂ Y₂ : C} {el er : ε} (h : el ↽ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂) [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    f ▷ X₂ ≫ Y₁ ◁ g ↞ X₁ ◁ g ≫ f ▷ Y₂
+    := (EC.eff_left_mover h).right_fwd g f hg.has_eff hf.has_eff
+
+theorem Effectful2.eff_right_bwd
+  {X₁ Y₁ X₂ Y₂ : C} {el er : ε} (h : el ↽ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂) [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    _ ◁ f ≫ g ▷ _ ↞ g ▷ _ ≫ _ ◁ f
+    := (EC.eff_left_mover h).left_fwd g f hg.has_eff hf.has_eff
+
+
+theorem Effectful2.eff_left_fwd_assoc_app
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ⇀ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    {h h' : Y₁ ⊗ Y₂ ⟶ Z} (hh : h ↠ h')
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    f ▷ X₂ ≫ Y₁ ◁ g ≫ h ↠ X₁ ◁ g ≫ f ▷ Y₂ ≫ h'
+  := by
+  rw [<-Category.assoc, <-Category.assoc]
+  exact refines_comp (eff_left_fwd he f g) hh
+
+theorem Effectful2.eff_left_fwd_assoc
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ⇀ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    (h : Y₁ ⊗ Y₂ ⟶ Z)
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    f ▷ X₂ ≫ Y₁ ◁ g ≫ h ↠ X₁ ◁ g ≫ f ▷ Y₂ ≫ h
+  := eff_left_fwd_assoc_app he f g (refines_refl h)
+
+theorem Effectful2.eff_right_fwd_assoc_app
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ⇀ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    {h h' : Y₂ ⊗ Y₁ ⟶ Z} (hh : h ↠ h')
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    _ ◁ f ≫ g ▷ _ ≫ h ↠ g ▷ _ ≫ _ ◁ f ≫ h'
+  := by
+  rw [<-Category.assoc, <-Category.assoc]
+  exact refines_comp (eff_right_fwd he f g) hh
+
+theorem Effectful2.eff_right_fwd_assoc
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ⇀ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    (h : Y₂ ⊗ Y₁ ⟶ Z)
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    _ ◁ f ≫ g ▷ _ ≫ h ↠ g ▷ _ ≫ _ ◁ f ≫ h
+  := eff_right_fwd_assoc_app he f g (refines_refl h)
+
+theorem Effectful2.eff_left_bwd_assoc_app
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ↽ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    {h h' : Y₁ ⊗ Y₂ ⟶ Z} (hh : h ↞ h')
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    f ▷ X₂ ≫ Y₁ ◁ g ≫ h ↞ X₁ ◁ g ≫ f ▷ Y₂ ≫ h'
+  := by
+  rw [<-Category.assoc, <-Category.assoc]
+  exact refines_comp (eff_left_bwd he f g) hh
+
+theorem Effectful2.eff_left_bwd_assoc
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ↽ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    (h : Y₁ ⊗ Y₂ ⟶ Z)
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    f ▷ X₂ ≫ Y₁ ◁ g ≫ h ↞ X₁ ◁ g ≫ f ▷ Y₂ ≫ h
+  := eff_left_bwd_assoc_app he f g (refines_refl h)
+
+theorem Effectful2.eff_right_bwd_assoc_app
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ↽ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    {h h' : Y₂ ⊗ Y₁ ⟶ Z} (hh : h ↞ h')
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    _ ◁ f ≫ g ▷ _ ≫ h ↞ g ▷ _ ≫ _ ◁ f ≫ h'
+  := by
+  rw [<-Category.assoc, <-Category.assoc]
+  exact refines_comp (eff_right_bwd he f g) hh
+
+theorem Effectful2.eff_right_bwd_assoc
+  {X₁ Y₁ X₂ Y₂ Z : C} {el er : ε} (he : el ↽ er)
+    (f : X₁ ⟶ Y₁) (g : X₂ ⟶ Y₂)
+    (h : Y₂ ⊗ Y₁ ⟶ Z)
+    [hf : EC.HasEff el f] [hg : EC.HasEff er g] :
+    _ ◁ f ≫ g ▷ _ ≫ h ↞ g ▷ _ ≫ _ ◁ f ≫ h
+  := eff_right_bwd_assoc_app he f g (refines_refl h)
